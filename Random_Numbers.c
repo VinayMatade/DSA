3d24c6f7315602ed2b74bbb95d99537cf24ff773
VinayMatade <vinaymatade@gmail.com>
2025-02-28 08:55:31 +0530

    read and write fix

--- Random_Numbers.c
+++ Random_Numbers.c
@@ -5,6 +5,13 @@
 
 int main()
 {
+    FILE *inputFile;
+    inputFile=fopen("input.txt", "w");
+    if (inputFile == NULL) 
+    {
+        printf("File not found\n");
+        return 1;
+    }
     int r,p,q,n;
     printf("enter the value of P, Q and N\n");
     scanf("%d%d%d",&p,&q,&n);
@@ -14,7 +21,9 @@ int main()
     {
         r= p+rand()%(q-p+1);
         printf("%d\n",r);
+        fprintf(inputFile, "%d ", r);
     }
+    fclose(inputFile);
     printf("Press any key to close\n");
     char a;
     scanf(" %c",&a);
